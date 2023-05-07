46ec60624aba420548ac563c2f34489cb3530401
Oleg <oksik.gavron88@gmail.com>
2023-05-07 16:41:27 +0300

    Homework 1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Шановний викладач бажаю вам всього найкращого і великої зарплати")
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+class Manager(Employee):
+    def __init__(self):
+        self.name = "Bob"
+        self.age = 46
+        self.salary = 100000
+        self.subordinates = 15
+
+class Developer(Employee):
+    def __init__(self):
+        self.name = "John"
+        self.age = 27
+        self.salary = 25000
+        self.program = "Phyton"
+
+a1 = Manager
+b1 = Developer
+
+print(a1)
+print(b1)
