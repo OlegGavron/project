4db008665ff0c483fbe6bd37e6bd11cc1ff928d9
Oleg <oksik.gavron88@gmail.com>
2023-05-18 17:21:43 +0300

    Homework 2

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-class Employee:
-    def __init__(self, name, age, salary):
-        self.name = name
-        self.age = age
-        self.salary = salary
+class Human:
+  def __init__(self, name):
+    self.name = name
 
-class Manager(Employee):
-    def __init__(self):
-        self.name = "Bob"
-        self.age = 46
-        self.salary = 100000
-        self.subordinates = 15
+class Auto:
+  def __init__(self, brand):
+    self.brand = brand
+    self.passengers = []
+  def add_passenger(self, human):
+    self.passengers.append(human)
 
-class Developer(Employee):
-    def __init__(self):
-        self.name = "John"
-        self.age = 27
-        self.salary = 25000
-        self.program = "Phyton"
+  def print_passengers(self):
+    if self.passengers != []:
+      print(f"В машині {self.brand} такі пасажири:")
+      for passenger in self.passengers:
+        print(passenger.name)
+    else:
+      print(f"В машині {self.brand} немає пасажирів. Всі втікли!")
 
-a1 = Manager
-b1 = Developer
+car = Auto('Nissan GTR')
+car.add_passenger(Human("Oleg"))
+car.add_passenger(Human("Igor"))
+car.add_passenger(Human("Vlad"))
+car.add_passenger(Human("Valentyn"))
+car.print_passengers()
 
-print(a1)
-print(b1)
+input("How many passangers you going to place in car?:")
+passangers = 5
+print("If you wrote 5:I Not much place in car")
+passengers = 4
+print("If you wrote 4:Car is full now")
+passengers = 3
+print("If you wrote 3:Cool")
+passengers = 2
+print("If you wrote 2:Only passanger and driver")
+passengers = 1
+print("If you wrote 1:Only driver")
